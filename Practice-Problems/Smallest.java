b0f484dd4b6bef2342d5d59f660f1ca752017e06
Nishant Nirmal <nirmalnishant645@gmail.com>
2020-01-31 13:15:09 +0530

    k smallest number in array

---
+++ Practice-Problems/Smallest.java
@@ -0,0 +1,35 @@
+/*
+Write a Java Program to find the k smallest elements in a given array.
+Elements in the array can be in any order.
+*/
+
+import java.util.*;
+
+public class Smallest {
+	public static void main (String[] args) {
+
+		int n, i, k;
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the length of array: ");
+		n = sc.nextInt();
+
+		int arr[] = new int[n];
+
+		System.out.println("Enter the elements of the array: ");		
+		for(i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		} 
+		
+		System.out.println("Enter the value of 'k': ");
+		k = sc.nextInt();
+
+		Arrays.sort(arr);
+
+		System.out.println("k smallest elements in the array are: ");
+		for(i = 0; i < k; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
