f583ef7efce657c1ba8eebef7988cf8c623a3fab
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-07-30 17:18:19 +0530

    Star Pattern

---
+++ Programming-Challenges/Fundamentals/src/com/challenges/StarPattern.java
@@ -0,0 +1,25 @@
+/*
+Write a program that displays the following pattern:
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+ */
+
+package com.challenges;
+
+public class StarPattern {
+    public static void main(String [] args) {
+
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ***** ");
+        System.out.println("*******");
+        System.out.println(" ***** ");
+        System.out.println("  ***  ");
+        System.out.println("   *   ");
+    }
+}
